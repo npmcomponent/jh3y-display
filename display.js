4663e84d29e4f36cdde453286d61cd09d4be88dc
jheytompkins <jh3y@users.noreply.github.com>
2014-03-15 19:32:21 +0000

    Major changes made to implement autoplay, pagination, navigation etc.

--- display.js
+++ display.js
@@ -47,10 +47,18 @@ function require(path, parent, orig) {
 require.modules = {};
 
 /**
- * Registered aliases.
+ * Main definitions.
  */
 
-require.aliases = {};
+require.mains = {};
+
+/**
+ * Define a main.
+ */
+
+require.main = function(name, path){
+  require.mains[name] = path;
+};
 
 /**
  * Resolve `path`.
@@ -67,7 +75,7 @@ require.aliases = {};
  */
 
 require.resolve = function(path) {
-  if (path.charAt(0) === '/') path = path.slice(1);
+  if ('/' == path.charAt(0)) path = path.slice(1);
 
   var paths = [
     path,
@@ -77,10 +85,15 @@ require.resolve = function(path) {
     path + '/index.json'
   ];
 
-  for (var i = 0; i < paths.length; i++) {
+  if (require.mains[path]) {
+    paths = [path + '/' + require.mains[path]];
+  }
+
+  for (var i = 0, len = paths.length; i < len; i++) {
     var path = paths[i];
-    if (require.modules.hasOwnProperty(path)) return path;
-    if (require.aliases.hasOwnProperty(path)) return require.aliases[path];
+    if (require.modules.hasOwnProperty(path)) {
+      return path;
+    }
   }
 };
 
@@ -101,7 +114,7 @@ require.normalize = function(curr, path) {
   curr = curr.split('/');
   path = path.split('/');
 
-  for (var i = 0; i < path.length; ++i) {
+  for (var i = 0, len = path.length; i < len; ++i) {
     if ('..' == path[i]) {
       curr.pop();
     } else if ('.' != path[i] && '' != path[i]) {
@@ -125,21 +138,6 @@ require.register = function(path, definition) {
 };
 
 /**
- * Alias a module definition.
- *
- * @param {String} from
- * @param {String} to
- * @api private
- */
-
-require.alias = function(from, to) {
-  if (!require.modules.hasOwnProperty(from)) {
-    throw new Error('Failed to alias "' + from + '", it does not exist');
-  }
-  require.aliases[to] = from;
-};
-
-/**
  * Return a require function relative to the `parent` path.
  *
  * @param {String} parent
@@ -148,7 +146,7 @@ require.alias = function(from, to) {
  */
 
 require.relative = function(parent) {
-  var p = require.normalize(parent, '..');
+  var root = require.normalize(parent, '..');
 
   /**
    * lastIndexOf helper.
@@ -178,15 +176,7 @@ require.relative = function(parent) {
   localRequire.resolve = function(path) {
     var c = path.charAt(0);
     if ('/' == c) return path.slice(1);
-    if ('.' == c) return require.normalize(p, path);
-
-    // resolve deps by returning
-    // the dep in the nearest "deps"
-    // directory
-    var segs = parent.split('/');
-    var i = lastIndexOf(segs, 'deps') + 1;
-    if (!i) i = 0;
-    path = segs.slice(0, i + 1).join('/') + '/deps/' + path;
+    if ('.' == c) return require.normalize(root, path);
     return path;
   };
 
@@ -200,13 +190,14 @@ require.relative = function(parent) {
 
   return localRequire;
 };
-
-
 require.register("display/index.js", function(exports, require, module){
 module.exports = display;
-function display(element) {
+function display(element, options) {
 	if (!(this instanceof display)) return new display(element);
 	this.element = element;
+	this._nav = (options && options.nav) ? true: false;
+	this._pagination = (options && options.pagination) ? true: false;
+	this._infinite = (options && options.infinite) ? true: false;
 	this._create();
 	for (var ext in this._extensions) {this._extensions[ext](this);}
 }
@@ -214,15 +205,39 @@ display.prototype._extensions = {}
 display.prototype._configurePanels = function () {
 	var display = this,
 		count,
+		panelLength = display.element.querySelectorAll('.display-panel').length,
 		currentPanel,
-		directions = {'nextElementSibling': 'data-coming', 'previousElementSibling': 'data-gone'};
-	for (direction in directions) {
-		currentPanel = display.element.querySelector('.current');
-		count = 1;
-		while (currentPanel[direction] !== null) {
-			currentPanel[direction].setAttribute(directions[direction], count);
-			count ++;
-			currentPanel = currentPanel[direction];
+		directions = {'nextElementSibling': 'data-coming', 'previousElementSibling': 'data-gone'},
+		cleanUp = function () {
+			for (direction in directions) {
+				currentPanel = display.element.querySelector('.display-panel.current');
+				count = 1;
+				while (currentPanel[direction] !== null && (currentPanel[direction].className.indexOf('display-panel') !== -1) && (currentPanel[direction].tagName.toUpperCase() === 'DIV' || currentPanel[direction].tagName.toUpperCase() === 'LI')) {
+					currentPanel[direction].setAttribute(directions[direction], count);
+					if (display._infinite && directions[direction] === 'data-coming') {
+						currentPanel[direction].removeAttribute('data-gone');
+					} else if (display._infinite && directions[direction] === 'data-gone') {
+						currentPanel[direction].removeAttribute('data-coming');
+					}
+					count ++;
+					currentPanel = currentPanel[direction];
+				}
+			}
+		};
+	cleanUp();
+	if (display._infinite && panelLength >= 3) {
+		if (display.element.querySelectorAll('.display-panel')[panelLength - 1].className.indexOf('current') !== -1) {
+			if (display._nav || display.element.querySelector('.display-nav-left') || display.element.querySelector('.display-nav-right')) {
+				display.element.insertBefore(display.element.firstElementChild, display.element.querySelector('.display-nav-left'));
+			} else if (display._pagination || display.element.querySelector('.display-pagination-pen')) {
+				display.element.insertBefore(display.element.firstElementChild, display.element.querySelector('.display-pagination-pen'));
+			} else {
+				display.element.appendChild(display.element.firstElementChild);
+			}
+			cleanUp();
+		} else if (display.element.firstElementChild.className.indexOf('current') !== -1) {
+			display.element.insertBefore(display.element.querySelectorAll('.display-panel')[panelLength - 1], display.element.firstElementChild);
+			cleanUp();
 		}
 	}
 }
@@ -232,27 +247,96 @@ display.prototype._create = function () {
 	display.element.className = display.element.className + ' display';
 	display.element.setAttribute('data-display', true);
 	[].forEach.call(panels, function (panel, index) {
-		if (panel.tagName.toLowerCase() === 'div') {
+		if (panel.tagName.toUpperCase() === 'DIV' || panel.tagName.toUpperCase() === 'LI') {
 			panel.className = (index === 0) ? panel.className + ' current display-panel': panel.className + ' display-panel';
+			panel.setAttribute('data-display-panel-index', index);
 		}
 	});
 	display._configurePanels();
-}
-display.prototype._move = function (direction, element) {
-	direction = (direction !== undefined) ? direction : 'next';
-	if (direction === 'previous' || direction === 'next') {
-		var prop = (direction === 'previous') ? 'previousElementSibling' : 'nextElementSibling',
-			attr = (direction === 'previous') ? 'data-gone' : 'data-coming',
-			display = this,
-			currentPanel = display.element.querySelector('.current');
-		if (currentPanel[prop] !== null) {
-			currentPanel.className = currentPanel.className.replace('current', '');
-			currentPanel.removeAttribute(attr);
-			currentPanel[prop].className = currentPanel[prop].className + ' current';
-			currentPanel[prop].removeAttribute(attr);
-			display._configurePanels();
-		}
+	if (this._nav) {
+		var navLeft = document.createElement('span'),
+			navRight = document.createElement('span');
+		navLeft.className = 'display-nav-left';
+		navRight.className = 'display-nav-right';
+		display.element.appendChild(navLeft);
+		display.element.appendChild(navRight);
+		navLeft.addEventListener('click', function (event) {
+			display.previous();
+		});
+		navRight.addEventListener('click', function (event) {
+			display.next();
+		});
 	}
+	panels = display.element.querySelectorAll('.display-panel');
+	if (this._pagination) {
+		var paginationPen = document.createElement('span');
+		paginationPen.className = 'display-pagination-pen';
+		for (var i = 0; i < panels.length; i++) {
+			var newPG = document.createElement('span');
+			newPG.className = 'PGIndicator';
+			newPG.setAttribute('data-display-index', i);
+			if (i === 0) {
+				newPG.className += ' current';
+			}
+			newPG.addEventListener('click', function () {
+				var currentPanel = display.element.querySelector('.display-panel.current'),
+					currentIndex = parseInt(currentPanel.getAttribute('data-display-panel-index'), 10),
+					reIndex = function () {
+						for (var i = panels.length - 1; i >= 0; i--) {
+							display.element.insertBefore(display.element.querySelector('[data-display-panel-index="' + i + '"]'), display.element.firstElementChild);
+						};
+						display._configurePanels();
+					},
+					PG = this;
+				reIndex();
+				setTimeout(function () {
+					display._move(parseInt(PG.getAttribute('data-display-index'), 10));
+				}, 250);
+			});
+			paginationPen.appendChild(newPG);
+		};
+		display.element.appendChild(paginationPen);
+	}
+}
+display.prototype._move = function (direction) {
+	var display = this,
+		currentPanel = display.element.querySelector('.display-panel.current'),
+		newDestination,
+		makeMove = function () {
+			currentPanel.className = currentPanel.className.replace('current', '');
+			currentPanel.removeAttribute('data-gone');
+			currentPanel.removeAttribute('data-coming');
+			newDestination.removeAttribute('data-gone');
+			newDestination.removeAttribute('data-coming');
+			newDestination.className += ' current';
+			if (display._pagination) {
+				display._configurePaginationIndicators();
+			}
+			display._configurePanels();
+		};
+	if (direction !== undefined && typeof(direction) === 'string' && (direction === 'next' || direction === 'previous')) {
+		var prop = (direction === 'previous') ? 'previousElementSibling' : 'nextElementSibling',
+			attr = (direction === 'previous') ? 'data-gone' : 'data-coming';
+		newDestination = currentPanel[prop];
+		if (newDestination !== null && newDestination.className.indexOf('display-panel') !== -1 && (newDestination.tagName.toUpperCase() === 'DIV' || newDestination.tagName.toUpperCase() === 'LI')) {
+			makeMove();
+		}
+	} else if (direction !== undefined && typeof(direction) === 'number' && direction >= 0) {
+		newDestination = display.element.querySelector('[data-display-panel-index="' + direction + '"]');
+		if (newDestination !== undefined) {
+			makeMove();
+		} else {
+			throw Error('display: ERROR index is out of bounds for this');
+		}
+	} else {
+		throw Error('display: ERROR invalid direction for moving.');
+	}
+}
+display.prototype._configurePaginationIndicators = function () {
+	var display = this,
+		currentIndex = parseInt(display.element.querySelector('.display-panel.current').getAttribute('data-display-panel-index'), 10);
+	display.element.querySelector('.PGIndicator.current').className = display.element.querySelector('.PGIndicator.current').className.replace("current", "");
+	display.element.querySelector('.PGIndicator[data-display-index="' + currentIndex + '"]').className += ' current';
 }
 display.prototype.next = function () {
 	this._move('next');
@@ -260,12 +344,7 @@ display.prototype.next = function () {
 display.prototype.previous = function () {
 	this._move('previous');
 }
-});
-
-
-
-
-require.alias("display/index.js", "display/index.js");if (typeof exports == "object") {
+});if (typeof exports == "object") {
   module.exports = require("display");
 } else if (typeof define == "function" && define.amd) {
   define(function(){ return require("display"); });
--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 module.exports = display;
-function display(element) {
+function display(element, options) {
 	if (!(this instanceof display)) return new display(element);
 	this.element = element;
+	this._nav = (options && options.nav) ? true: false;
+	this._pagination = (options && options.pagination) ? true: false;
+	this._infinite = (options && options.infinite) ? true: false;
+	this._autoPlay = (options && options.autoPlay) ? true: false;
+	this._autoPlayDuration = (options && options.autoPlayDuration) ? parseInt(options.autoPlayDuration, 10): 5000;
 	this._create();
 	for (var ext in this._extensions) {this._extensions[ext](this);}
 }
@@ -9,15 +14,38 @@ display.prototype._extensions = {}
 display.prototype._configurePanels = function () {
 	var display = this,
 		count,
+		panelLength = display.element.querySelectorAll('.display-panel').length,
+		lastPanel = display.element.querySelectorAll('.display-panel')[panelLength - 1],
 		currentPanel,
-		directions = {'nextElementSibling': 'data-coming', 'previousElementSibling': 'data-gone'};
-	for (direction in directions) {
-		currentPanel = display.element.querySelector('.current');
-		count = 1;
-		while (currentPanel[direction] !== null) {
-			currentPanel[direction].setAttribute(directions[direction], count);
-			count ++;
-			currentPanel = currentPanel[direction];
+		directions = {'nextElementSibling': 'data-coming', 'previousElementSibling': 'data-gone'},
+		cleanUp = function () {
+			for (direction in directions) {
+				currentPanel = display.element.querySelector('.display-panel.current');
+				count = 1;
+				while (currentPanel[direction] !== null && (currentPanel[direction].className.indexOf('display-panel') !== -1) && (currentPanel[direction].tagName.toUpperCase() === 'DIV' || currentPanel[direction].tagName.toUpperCase() === 'LI')) {
+					currentPanel[direction].setAttribute(directions[direction], count);
+					if (directions[direction] === 'data-coming') {
+						currentPanel[direction].removeAttribute('data-gone');
+					} else if (directions[direction] === 'data-gone') {
+						currentPanel[direction].removeAttribute('data-coming');
+					}
+					count ++;
+					currentPanel = currentPanel[direction];
+				}
+			}
+		};
+	cleanUp();
+	if (display._infinite && panelLength >= 3) {
+		if (lastPanel.className.indexOf('current') !== -1) {
+			if (display.element.lastElementChild.className.indexOf('display-panel') !== -1) {
+				display.element.appendChild(display.element.firstElementChild);
+			} else {
+				display.element.insertBefore(display.element.firstElementChild, lastPanel.nextElementSibling);
+			}
+			cleanUp();
+		} else if (display.element.firstElementChild.className.indexOf('current') !== -1) {
+			display.element.insertBefore(lastPanel, display.element.firstElementChild);
+			cleanUp();
 		}
 	}
 }
@@ -27,27 +55,122 @@ display.prototype._create = function () {
 	display.element.className = display.element.className + ' display';
 	display.element.setAttribute('data-display', true);
 	[].forEach.call(panels, function (panel, index) {
-		if (panel.tagName.toLowerCase() === 'div') {
+		if (panel.tagName.toUpperCase() === 'DIV' || panel.tagName.toUpperCase() === 'LI') {
 			panel.className = (index === 0) ? panel.className + ' current display-panel': panel.className + ' display-panel';
+			panel.setAttribute('data-display-panel-index', index);
 		}
 	});
 	display._configurePanels();
-}
-display.prototype._move = function (direction, element) {
-	direction = (direction !== undefined) ? direction : 'next';
-	if (direction === 'previous' || direction === 'next') {
-		var prop = (direction === 'previous') ? 'previousElementSibling' : 'nextElementSibling',
-			attr = (direction === 'previous') ? 'data-gone' : 'data-coming',
-			display = this,
-			currentPanel = display.element.querySelector('.current');
-		if (currentPanel[prop] !== null) {
-			currentPanel.className = currentPanel.className.replace('current', '');
-			currentPanel.removeAttribute(attr);
-			currentPanel[prop].className = currentPanel[prop].className + ' current';
-			currentPanel[prop].removeAttribute(attr);
-			display._configurePanels();
-		}
+	if (this._nav) {
+		var navLeft = document.createElement('i'),
+			navRight = document.createElement('i');
+		navLeft.className = 'display-nav-left';
+		navRight.className = 'display-nav-right';
+		display.element.appendChild(navLeft);
+		display.element.appendChild(navRight);
+		navLeft.addEventListener('click', function (event) {
+			display.previous();
+		});
+		navRight.addEventListener('click', function (event) {
+			display.next();
+		});
 	}
+	panels = display.element.querySelectorAll('.display-panel');
+	if (this._pagination) {
+		var reIndex = function () {
+			for (var i = panels.length - 1; i >= 0; i--) {
+				display.element.insertBefore(display.element.querySelector('[data-display-panel-index="' + i + '"]'), display.element.firstElementChild);
+			};
+			display._configurePanels();
+		},
+			paginationIndicator,
+			paginationPen = document.createElement('span');
+		paginationPen.className = 'display-pagination-pen';
+		for (var i = 0; i < panels.length; i++) {
+			var newIndicator = document.createElement('i');
+			newIndicator.className = 'display-pagination-indicator';
+			newIndicator.setAttribute('data-display-index', i);
+			if (i === 0) {
+				newIndicator.className += ' current';
+			}
+			newIndicator.addEventListener('click', function () {
+				paginationIndicator = this;
+				display._move(parseInt(paginationIndicator.getAttribute('data-display-index'), 10));
+			});
+			paginationPen.appendChild(newIndicator);
+		};
+		display.element.appendChild(paginationPen);
+	}
+	if (display._autoPlay) {
+		display.autoPlay(true, display._autoPlayDuration, 'next');
+	}
+}
+display.prototype.autoPlay = function (autoPlay, speed, direction) {
+	var display = this;
+	if (autoPlay) {
+		speed = (speed && typeof(speed) === 'number') ? speed: 5000;
+		direction = (direction && typeof(direction) === 'string') ? direction: 'next'; 
+		window.display.playInterval = window.setInterval(function () {
+			display._move(direction);
+		}, speed);
+		display._autoPlay = true;
+		display._autoPlayID = window.display.playInterval;
+	} else {
+		window.clearInterval(display._autoPlayID);
+		display._autoPlay = false;
+	}
+}
+display.prototype._move = function (direction) {
+	var display = this,
+		currentPanel = display.element.querySelector('.display-panel.current'),
+		panels = display.element.querySelectorAll('.display-panel'),
+		newDestination,
+		reIndex = function () {
+			for (var i = panels.length - 1; i >= 0; i--) {
+				display.element.insertBefore(display.element.querySelector('[data-display-panel-index="' + i + '"]'), display.element.firstElementChild);
+			};
+			display._configurePanels();
+		},
+		makeMove = function () {
+			currentPanel.className = currentPanel.className.replace('current', '');
+			currentPanel.removeAttribute('data-gone');
+			currentPanel.removeAttribute('data-coming');
+			newDestination.removeAttribute('data-gone');
+			newDestination.removeAttribute('data-coming');
+			newDestination.className += ' current';
+			if (display._pagination) {
+				display._configurePaginationIndicators();
+			}
+			display._configurePanels();
+		};
+	if (direction !== undefined && typeof(direction) === 'string' && (direction === 'next' || direction === 'previous')) {
+		var prop = (direction === 'previous') ? 'previousElementSibling' : 'nextElementSibling',
+			attr = (direction === 'previous') ? 'data-gone' : 'data-coming';
+		newDestination = currentPanel[prop];
+		if (newDestination !== null && newDestination.className.indexOf('display-panel') !== -1 && (newDestination.tagName.toUpperCase() === 'DIV' || newDestination.tagName.toUpperCase() === 'LI')) {
+			makeMove();
+		} else if (display._autoPlay) {
+			display._move(0);
+		}
+	} else if (direction !== undefined && typeof(direction) === 'number' && direction >= 0) {
+		newDestination = display.element.querySelector('[data-display-panel-index="' + direction + '"]');
+		if (newDestination !== undefined) {
+			reIndex();
+			setTimeout(function () {
+				makeMove();
+			}, 250);
+		} else {
+			throw Error('display: ERROR index is out of bounds for this');
+		}
+	} else {
+		throw Error('display: ERROR invalid direction for moving.');
+	}
+}
+display.prototype._configurePaginationIndicators = function () {
+	var display = this,
+		currentIndex = parseInt(display.element.querySelector('.display-panel.current').getAttribute('data-display-panel-index'), 10);
+	display.element.querySelector('.display-pagination-indicator.current').className = display.element.querySelector('.display-pagination-indicator.current').className.replace("current", "");
+	display.element.querySelector('.display-pagination-indicator[data-display-index="' + currentIndex + '"]').className += ' current';
 }
 display.prototype.next = function () {
 	this._move('next');
